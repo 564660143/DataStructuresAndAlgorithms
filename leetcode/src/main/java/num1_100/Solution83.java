c1cbe490cc3d2164f3c6437cc69e579d41def69c
qiyexue <564660143@qq.com>
2018-11-27 14:20:13 +0800

    2. 两数相加 203. 移除链表元素

--- leetcode/src/main/java/num1_100/Solution83.java
+++ leetcode/src/main/java/num1_100/Solution83.java
@@ -4,7 +4,7 @@ package num1_100;
  * 83. 删除排序链表中的重复元素
  *
  */
-class Solution {
+class Solution83 {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) {
             return null;
