3ffa36eea05ea5093619f093c1f1d089d0e30459
qiyexue <564660143@qq.com>
2018-12-04 14:12:40 +0800

    11. 盛最多水的容器

---
+++ leetcode/src/main/java/num1_100/Solution11.java
@@ -0,0 +1,22 @@
+package num1_100;
+
+/**
+ * 11. 盛最多水的容器
+ */
+class Solution11 {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                maxArea = Math.max(maxArea, height[left] * (right - left));
+                left++;
+            } else {
+                maxArea = Math.max(maxArea, height[right] * (right - left));
+                right--; 
+            }
+        }
+        
+        return maxArea;
+    }
+}
