3936bfb3ee233a19234dd301cde0a98832ffba55
qiyexue <564660143@qq.com>
2018-12-25 10:55:42 +0800

    40. 组合总和 II

--- leetcode/src/main/java/num1_100/Solution40.java
+++ leetcode/src/main/java/num1_100/Solution40.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 39. 组合总和
+ * 40. 组合总和 II
  */
-class Solution39 {
+class Solution40 {
     List<List<Integer>> result = new ArrayList<>();
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         if (candidates == null || candidates.length < 1 || target < 0) {
             return result;
         }
@@ -17,29 +17,31 @@ class Solution39 {
         // 排序
         Arrays.sort(candidates);
         findTarget(candidates, target, 0, new ArrayList<>());
-
         return result;
     }
 
-    // 从candidates的索引为index位之后查找和为target的组合
-    private void findTarget(int[] candidates, int target, int index, List<Integer> temp){
+    private void findTarget(int[] candidates, int target, int index, List<Integer> temp) {
         if (target == 0) {
             result.add(new ArrayList<>(temp));
 //            System.out.println("get a result" + temp);
             return;
         }
 
-        for (int i = index; i < candidates.length && candidates[i] <= target; i++) {
+        for (int i = index; i < candidates.length && target >= candidates[i]; i++) {
+            // 剔除重复元素
+            if (i > index && candidates[i] == candidates[i - 1]){
+                continue;
+            }
             temp.add(candidates[i]);
-//            System.out.println("use : " + candidates[i] + " --  temp : " + temp);
-            findTarget(candidates, target - candidates[i], i, temp);
-            //
+//            System.out.println("index :" + i +  "--use : " + candidates[i] + " --  temp : " + temp);
+            findTarget(candidates, target - candidates[i], i + 1, temp);
+            // 回溯
             temp.remove(temp.size() - 1);
         }
     }
 
 //    public static void main(String[] args) {
-//        new Solution39().combinationSum(new int[]{2, 3, 6, 7}, 7);
+//        new Solution40().combinationSum2(new int[]{10,1,2,7,6,1,5}, 8);
 //    }
 
 }
