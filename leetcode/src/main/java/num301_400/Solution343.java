2a9bdc7284b255e6aca52f6863200537090b4c6b
xuxiumeng <564660143@qq.com>
2019-01-07 17:05:42 +0800

    343. 整数拆分

---
+++ leetcode/src/main/java/num301_400/Solution343.java
@@ -0,0 +1,24 @@
+package num301_400;
+
+/**
+ *  343. 整数拆分
+ *  拆出的3越多乘积越大, 剩余为4时拆成2 * 2时值最大, 所以4不需要拆分
+ */
+class Solution343 {
+    public int integerBreak(int n) {
+       if (n == 2) {
+           return 1;
+       }
+       if (n == 3) {
+           return 2;
+       }
+       int result = 1;
+       // n = 4 时拆分的最大值为2 * 2
+       while (n > 4) {
+           result = result * 3;
+           n = n - 3;
+       }
+
+       return result * n;
+    }
+}
